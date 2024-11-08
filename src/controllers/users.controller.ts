748d43172d31703ca1590011f90e9af9ece74d8b
unknown <brenoalmeidasantos1@gmail.com>
2024-11-08 14:22:17 -0300

    first commit

---
+++ src/controllers/users.controller.ts
@@ -0,0 +1,72 @@
+import { NextFunction, Request, Response } from "express"
+import { getFirestore } from "firebase-admin/firestore"
+import { ValidationError } from "../errors/validation.error";
+import { NotFoundError } from "../errors/not-found.error";
+
+type User = {
+  id: number;
+  nome: string;
+  email: string;
+}
+
+export class UsersController {
+  static async getAll(req: Request, res: Response, next: NextFunction) {
+    const snapshot = await getFirestore().collection("users").get();
+    const users = snapshot.docs.map(doc => {
+      return {
+        id: doc.id,
+        ...doc.data()
+      }
+    })
+    res.send(users)
+  }
+
+  static async getById(req: Request, res: Response, next: NextFunction) {
+    let userId = req.params.id
+    const doc = await getFirestore().collection("users").doc(userId).get()
+
+    if (doc.exists) {
+      res.send({
+        id: doc.id,
+        ...doc.data()
+      })
+    } else {
+      throw new NotFoundError("Usuario não encontrado")
+    }
+  }
+
+  static async save(req: Request, res: Response, next: NextFunction) {
+    let user = req.body
+
+
+    const userSalvo = await getFirestore().collection("users").add(user)
+    res.status(201).send({
+      message: `Usuário ${userSalvo.id} criado com sucesso!`
+    })
+  }
+
+
+  static async update(req: Request, res: Response, next: NextFunction) {
+    let userId = req.params.id
+    let user = req.body as User;
+    let docRef = getFirestore().collection("users").doc(userId)
+
+    if ((await docRef.get()).exists) {
+      await docRef.set({
+        nome: user.nome,
+        email: user.email
+      })
+      res.send({
+        message: "Usuário alterado com sucesso!"
+      })
+    } else {
+      throw new NotFoundError("Usuário nao encontrado!")
+    }
+  }
+
+  static async delete(req: Request, res: Response) {
+    let userId = req.params.id
+    await getFirestore().collection("users").doc(userId).delete()
+    res.status(204).end()
+  }
+}
---
+++ src/routes/users.route.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import { UsersController } from "../controllers/users.controller";
+import asyncHandler from "express-async-handler"
+import { celebrate, Joi, Segments } from "celebrate";
+
+export const userRoutes = express.Router()
+
+userRoutes.get("/users", asyncHandler(UsersController.getAll));
+userRoutes.get("/users/:id", asyncHandler(UsersController.getById))
+userRoutes.post("/users", celebrate({
+  [Segments.BODY]: Joi.object().keys({
+    nome: Joi.string().required(),
+  })
+}), asyncHandler(UsersController.save))
+userRoutes.put("/users/:id", asyncHandler(UsersController.update))
+userRoutes.delete("/users/:id", asyncHandler(UsersController.delete))
