02de03e48e07215269da84b9e8a81486a4667fcd
Breno Almeida <brenoalmeidasantos1@gmail.com>
2024-12-08 20:28:58 -0300

    trying fix login error

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,23 +1,38 @@
+import { FirebaseError } from "firebase/app";
 import { EmailAlreadyExistsError } from "../errors/email-already-exist.error";
+import { UnauthorizedError } from "../errors/unauthorized.error";
 import { User } from "../models/user.model";
-import { getAuth, UserRecord } from "firebase-admin/auth"
+import { FirebaseAuthError, getAuth, UserRecord } from "firebase-admin/auth"
 import { getAuth as getFirebaseAuth, signInWithEmailAndPassword, UserCredential } from "firebase/auth"
 
+
 export class AuthService {
+
     async create(user: User): Promise<UserRecord> {
-        return getAuth().createUser({
-            email: user.email,
-            password: user.password,
-            displayName: user.nome,
-        }).catch(err => {
-            if (err.code === "auth/email-already-exists") {
+
+        try {
+            return getAuth().createUser({
+                email: user.email,
+                password: user.password,
+                displayName: user.nome,
+            })
+        } catch (err) {
+            if (err instanceof FirebaseAuthError && err.code === "auth/email-already-exists") {
                 throw new EmailAlreadyExistsError();
             }
             throw err;
-        })
+        }
     }
 
-    login(email: string, password: string): Promise<UserCredential> {
-        return signInWithEmailAndPassword(getFirebaseAuth(), email, password)
+    async login(email: string, password: string): Promise<UserCredential> {
+        return await signInWithEmailAndPassword(getFirebaseAuth(), email, password)
+            .catch(err => {
+                if (err instanceof FirebaseError) {
+                    if (err.code === "auth/invalid-credential") {
+                        throw new UnauthorizedError();
+                    }
+                }
+                throw err
+            })
     }
 }
