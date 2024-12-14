ef3e5fffad38eba8c56cdc62daf35d436809bae6
Breno Almeida <brenoalmeidasantos1@gmail.com>
2024-12-14 14:41:25 -0300

    create companySchema on models and added on routes

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,18 +1,18 @@
-import { NextFunction, Request, Response } from "express"
+import { Request, Response } from "express"
 import { User } from "../models/user.model";
 import { UserService } from "../services/users.service";
 
 export class UsersController {
-  static async getAll(req: Request, res: Response, next: NextFunction) {
+  static async getAll(req: Request, res: Response) {
     res.send(await new UserService().getAll())
   }
 
-  static async getById(req: Request, res: Response, next: NextFunction) {
+  static async getById(req: Request, res: Response) {
     let userId = req.params.id
     res.send(await new UserService().getById(userId))
   }
 
-  static async save(req: Request, res: Response, next: NextFunction) {
+  static async save(req: Request, res: Response) {
     await new UserService().save(req.body)
     res.status(201).send({
       message: `Usuário criado com sucesso!`
@@ -20,7 +20,7 @@ export class UsersController {
   }
 
 
-  static async update(req: Request, res: Response, next: NextFunction) {
+  static async update(req: Request, res: Response) {
     let userId = req.params.id
     let user = req.body as User
     await new UserService().update(userId, user)
--- src/models/company.model.ts
+++ src/models/company.model.ts
@@ -1,5 +1,4 @@
-
-
+import { Joi } from "celebrate"
 
 export type Company = {
   id?: string
@@ -14,3 +13,16 @@ export type Company = {
   taxaEntrega: string
   ativa: boolean
 }
+
+export const companySchema = Joi.object().keys({
+  logomarca: Joi.string(),
+  cpfCnpj: Joi.string().required(),
+  razaoSocial: Joi.string().required(),
+  nomeFantasia: Joi.string().required(),
+  telefone: Joi.string().required(),
+  horarioFuncionamento: Joi.string().required(),
+  endereco: Joi.string().required(),
+  localizacao: Joi.string().required(),
+  taxaEntrega: Joi.number().required(),
+  ativa: Joi.boolean().default(true)
+})
